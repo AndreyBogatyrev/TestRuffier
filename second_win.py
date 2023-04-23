55cb960759df750d5bd536cd181ee565270bb9ab
Avershoot <bobscrendlek@mail.ru>
2023-04-23 20:29:44 +1000

    первый этап

--- second_win.py
+++ second_win.py
@@ -1,2 +1,57 @@
-name = 'Максим'
-print(f"Тест от {name}а!!")
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QLabel, QVBoxLayout, QHBoxLayout, QMessageBox
+from PyQt5.QtCore import Qt
+import instr as T
+
+app = QApplication([])
+
+window = QWidget()
+window.move(T.win_x, T.win_y)
+window.resize(T.win_width, T.win_height)
+window.setWindowTitle(T.txt_title)
+
+# winner = QLabel('Так можно создать текст')
+# winner.setText('Создать новый текст')
+# winner.text() -- получить текст
+
+# button = QPushButton('Текст тут')
+
+# v_line = QVBoxLayout()
+# v_line.addWidget(winner, alignment = Qt.AlignCenter)
+# v_line.addWidget(button, alignment = Qt.AlignCenter)
+
+# window.setLayout(v_line)
+
+LV1 = QVBoxLayout()
+fioText = QLabel(T.txt_name)
+ageText = QLabel(T.txt_age)
+inst1 = QLabel(T.txt_test1)
+inst2 = QLabel(T.txt_test2)
+inst3 = QLabel(T.txt_test3)
+
+buttoninst1 = QPushButton(T.txt_starttest1)
+buttoninst2 = QPushButton(T.txt_starttest2)
+buttoninst3 = QPushButton(T.txt_starttest3)
+buttonresult = QPushButton(T.txt_sendresults)
+
+LV1.addWidget(fioText, alignment=Qt.AlignLeft)
+LV1.addWidget(ageText, alignment=Qt.AlignLeft)
+LV1.addWidget(inst1, alignment=Qt.AlignLeft)
+LV1.addWidget(inst2, alignment=Qt.AlignLeft)
+LV1.addWidget(inst3, alignment=Qt.AlignLeft)
+LV1.addWidget(buttoninst1, alignment=Qt.AlignLeft)
+LV1.addWidget(buttoninst2, alignment=Qt.AlignLeft)
+LV1.addWidget(buttoninst3, alignment=Qt.AlignLeft)
+LV1.addWidget(buttonresult, alignment=Qt.AlignLeft)
+
+LV2 = QVBoxLayout()
+timeText = QLabel(T.txt_timer)
+LV2.addWidget(timeText, alignment=Qt.AlignCenter)
+
+main_line = QHBoxLayout()
+main_line.addLayout(LV1)
+main_line.addLayout(LV2)
+
+window.setLayout(main_line)
+
+window.show()
+app.exec_()
