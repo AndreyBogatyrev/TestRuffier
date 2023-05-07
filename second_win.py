4164fb3a86f5381c2703ed63155eeece0c6cc01e
Avershoot <bobscrendlek@mail.ru>
2023-05-07 15:56:21 +1000

    asd

--- second_win.py
+++ second_win.py
@@ -1,4 +1,4 @@
-from PyQt5.QtCore import Qt, QTime, QTimer
+from PyQt5.QtCore import Qt, QTimer, QTime
 from PyQt5.QtWidgets import QWidget, QLabel, QVBoxLayout, QPushButton, QLineEdit, QHBoxLayout
 from PyQt5.QtGui import QFont
 import instr as S
@@ -7,7 +7,6 @@ from final_win import FinalWin
 class TestWin(QWidget):
     def __init__(self):
         super().__init__()
-        self.timer = QTimer()
         self.set_appear()
         self.inituI()
         self.connection()
@@ -19,14 +18,12 @@ class TestWin(QWidget):
         self.move(S.win_x,S.win_y)
 
     def inituI(self):
+        self.LV1 = QVBoxLayout()
         self.fioText = QLabel(S.txt_name)
         self.ageText = QLabel(S.txt_age)
         self.inst1 = QLabel(S.txt_test1)
         self.inst2 = QLabel(S.txt_test2)
         self.inst3 = QLabel(S.txt_test3)
-        self.timeText = QLabel(S.txt_timer)
-        self.timeText.setFont(QFont("Times",36,QFont.Bold))
-
 
         self.buttoninst1 = QPushButton(S.txt_starttest1)
         self.buttoninst2 = QPushButton(S.txt_starttest2)
@@ -39,8 +36,8 @@ class TestWin(QWidget):
         self.edit2 = QLineEdit(S.txt_hinttest2)
         self.edit3 = QLineEdit(S.txt_hinttest3)
 
+        self.timer = QTimer()
 
-        self.LV1 = QVBoxLayout()
         self.LV1.addWidget(self.fioText, alignment=Qt.AlignLeft)
         self.LV1.addWidget(self.editName, alignment=Qt.AlignLeft)
         self.LV1.addWidget(self.ageText, alignment=Qt.AlignLeft)
@@ -50,77 +47,99 @@ class TestWin(QWidget):
         self.LV1.addWidget(self.edit1, alignment=Qt.AlignLeft)
         self.LV1.addWidget(self.inst2, alignment=Qt.AlignLeft)
         self.LV1.addWidget(self.buttoninst2, alignment=Qt.AlignLeft)
+        self.LV1.addWidget(self.edit2, alignment=Qt.AlignLeft)
         self.LV1.addWidget(self.inst3, alignment=Qt.AlignLeft)
         self.LV1.addWidget(self.buttoninst3, alignment=Qt.AlignLeft)
-        self.LV1.addWidget(self.edit2, alignment=Qt.AlignLeft)
         self.LV1.addWidget(self.edit3, alignment=Qt.AlignLeft)
         self.LV1.addWidget(self.buttonresult, alignment=Qt.AlignLeft)
 
+        self.LV2 = QVBoxLayout()
+        self.timeText = QLabel(S.txt_timer)
+        self.timeText.setFont(QFont('Times', 36, QFont.Bold))
+        self.LV2.addWidget(self.timeText, alignment=Qt.AlignCenter)
+
         main_line = QHBoxLayout()
         main_line.addLayout(self.LV1)
-        main_line.addWidget(self.timeText, alignment=Qt.AlignCenter)
+        main_line.addLayout(self.LV2)
 
         self.setLayout(main_line)
 
     def connection(self):
+        self.buttoninst1.clicked.connect(self.timer_test1)
+        self.buttoninst2.clicked.connect(self.timer_test2)
+        self.buttoninst3.clicked.connect(self.timer_test3)
+
         self.buttonresult.clicked.connect(self.next_click)
-        self.buttoninst1.clicked.connect(self.test1)
-        self.buttoninst2.clicked.connect(self.test2)
-        self.buttoninst3.clicked.connect(self.test3)
+        self.editName.editingFinished.connect(self.SetName)
+        self.editAge.editingFinished.connect(self.SetAge)
+        self.edit1.editingFinished.connect(self.SetRes1)
+        self.edit2.editingFinished.connect(self.SetRes2)
+        self.edit3.editingFinished.connect(self.SetRes3)
 
-    def test3(self):
-        self.time = QTime(0,0,0)
-        self.timeText.setText("00:00:00")
-        self.timer.timeout.connect(self.last_test)
-        self.timer.start(1000)
-
-    def test1(self):
-        self.time = QTime(0,0,15)
-        self.timeText.setText("00:00:15")
-        self.timer.timeout.connect(self.change_time)
-        self.timer.start(1000)
-
-    def test2(self):
-        self.sit = 0
-        self.timeText.setText(str(self.sit))
-        self.timer.timeout.connect(self.add_sit)
-        self.timer.start(1500)
-
-    def last_test(self):
-        if self.time.toString("hh:mm:ss") == "00:01:00":
-            self.timer.stop()
-        else:
-            self.time = self.time.addSecs(1)
-            if self.time.second() <=15 or self.time.second() >=45:
-                self.timeText.setStyleSheet("color:rgb(0,255,0)")
-            else:
-                self.timeText.setStyleSheet("color:rgb(0,0,0)")
-
-            self.timeText.setText(self.time.toString("hh:mm:ss"))
-            self.timer.start(1000)
-
-    def add_sit(self):
-        self.timeText.setStyleSheet("color:rgb(0,0,0)")
-        if self.sit == 30:
-            self.timer.stop()
-        else:
-            self.sit += 1
-            self.timeText.setText(str(self.sit))
-            self.timer.start(1500)
-
-    def change_time(self):
-        self.timeText.setStyleSheet("color:rgb(0,0,0)")
-        if self.time.toString("hh:mm:ss") == "00:00:00":
-            self.timer.stop()
-        else:
-            self.time = self.time.addSecs(-1)
-            self.timeText.setText(self.time.toString("hh:mm:ss"))
-            self.timer.start(1000)
-        
     def next_click(self):
         self.hide()
-        self.tw = FinalWin(self.editName.text,
-                           int(self.editAge.text()),
-                           (int(self.edit1.text())+
-                           int(self.edit2.text())+
-                           int(self.edit3.text())))
+        self.tw = FinalWin(self.Name, self.age, self.edit1, self.edit2, self.edit3)
+
+    def SetAge(self):
+        self.age = int(self.editAge.text())
+    def SetRes1(self):
+        self.Res1 = int(self.edit1.text())
+    def SetRes2(self):
+        self.Res2 = int(self.edit2.text())
+    def SetRes3(self):
+        self.Res3 = int(self.edit3.text())
+    
+    def SetName(self):
+        self.Name = self.editName.text()
+
+    def timer_test1(self):
+        global time
+        time = QTime(0, 1, 0)
+        self.timer.timeout.connect(self.timer1Event)
+        self.timer.start(1000) # 1000
+
+    def timer_test2(self):
+        global time
+        time = QTime(0, 0, 30)
+        self.timer.timeout.connect(self.timer2Event)
+        self.timer.start(1500) # 1500
+
+    def timer_test3(self):
+        global time
+        time = QTime(0, 1, 0)
+        self.timer.timeout.connect(self.timer3Event)
+        self.timer.start(1000) # 1000
+
+    def timer1Event(self):
+        global time
+        time = time.addSecs(-1)
+        self.timeText.setText(time.toString('hh:mm:ss'))
+        self.timeText.setStyleSheet('color: rgb(0, 0, 0)')
+        if time.toString('hh:mm:ss') == '00:00:00':
+            self.timer.stop()
+            self.timer.timeout.disconnect()
+
+    def timer2Event(self):
+        global time
+        time = time.addSecs(-1)
+        self.timeText.setText(time.toString('hh:mm:ss')[6:8])
+        self.timeText.setStyleSheet('color: rgb(0, 0, 0)')
+        if self.timeText.text() == '00':
+            self.timer.stop()
+            self.timer.timeout.disconnect()
+
+    def timer3Event(self):
+        global time
+        time = time.addSecs(-1)
+        self.timeText.setText(time.toString('hh:mm:ss'))
+
+        if int(time.toString('hh:mm:ss')[6:8]) >= 45:
+            self.timeText.setStyleSheet('color: rgb(0, 255, 0)')
+        elif int(time.toString('hh:mm:ss')[6:8]) <= 15:
+            self.timeText.setStyleSheet('color: rgb(0, 255, 0)')
+        else:
+            self.timeText.setStyleSheet('color: rgb(0, 0, 0)')
+        
+        if time.toString('hh:mm:ss') == '00:00:00':
+            self.timer.stop()
+            self.timer.timeout.disconnect()
